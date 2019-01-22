e67e330648589b4797617ec98c5a8bb0a84f9c7e
zawazawazawazawa <bosse.de.poulet@gmail.com>
2019-01-23 08:28:14 +0900

    wanna use class ...

--- GetCompetitivePricingForASIN.py
+++ GetCompetitivePricingForASIN.py
@@ -11,64 +11,113 @@ import six
 import xml.etree.ElementTree as ET
 import os
 
-ASIN_list = ['B07MTZHCKF', 'B07H4KJTC9', 'B01KZB4FF4','B07F85WTS7']
-
-
 AMAZON_CREDENTIAL = {
     'SELLER_ID': os.environ['SELLER_ID'],
     'ACCESS_KEY_ID': os.environ['ACCESS_KEY_ID'],
     'ACCESS_SECRET': os.environ['ACCESS_SECRET']
 }
 
-DOMAIN = 'mws.amazonservices.jp'
-ENDPOINT = '/Products/2011-10-01'
+ASIN_list = ['B07MTZHCKF', 'B07H4KJTC9', 'B01KZB4FF4','B07F85WTS7']
 
-def datetime_encode(dt):
-    return dt.strftime('%Y-%m-%dT%H:%M:%SZ')
+class Product:
+    # 全APIで使う変数
+    DOMAIN   = 'mws.amazonservices.jp'
+    ENDPOINT = '/Products/2011-10-01'
 
-timestamp = datetime_encode(datetime.datetime.utcnow())
+    def datetime_encode(dt):
+        return dt.strftime('%Y-%m-%dT%H:%M:%SZ')
 
-data = {
-    'AWSAccessKeyId': AMAZON_CREDENTIAL['ACCESS_KEY_ID'],
-    'Action'          : 'GetCompetitivePricingForASIN',
-    'MarketplaceId'   : 'A1VC38T7YXB528',
-    'SellerId'        : AMAZON_CREDENTIAL['SELLER_ID'],
-    'SignatureMethod' : 'HmacSHA256',
-    'SignatureVersion': '2',
-    'Timestamp'       : timestamp,
-    'Version'         : '2011-10-01'
-}
+    timestamp = datetime_encode(datetime.datetime.utcnow())
 
-for num in range(len(ASIN_list)):
-    index = str(num + 1)
-    data['ASINList.ASIN.' + index] = ASIN_list[num]
+    data = {
+        'AWSAccessKeyId'  : AMAZON_CREDENTIAL['ACCESS_KEY_ID'],
+        # 'Action'          : 'GetCompetitivePricingForASIN',
+        'MarketplaceId'   : 'A1VC38T7YXB528',
+        'SellerId'        : AMAZON_CREDENTIAL['SELLER_ID'],
+        'SignatureMethod' : 'HmacSHA256',
+        'SignatureVersion': '2',
+        'Timestamp'       : timestamp,
+        'Version'         : '2011-10-01'
+    }
 
-query_string = '&'.join('{}={}'.format(
-    n, urllib.parse.quote(v, safe='')) for n, v in sorted(data.items()))
+    def __init__(self, asin_list):
+        self.asin_list = asin_list
 
-canonical = "{}\n{}\n{}\n{}".format(
-    'POST', DOMAIN, ENDPOINT, query_string
-)
+    def enumerate_param(self, param, values):
+        """
+            Builds a dictionary of an enumerated parameter.
+            Takes any iterable and returns a dictionary.
+            ie.
+            enumerate_param('MarketplaceIdList.Id', (123, 345, 4343))
+            returns
+            {
+                MarketplaceIdList.Id.1: 123,
+                MarketplaceIdList.Id.2: 345,
+                MarketplaceIdList.Id.3: 4343
+            }
+        """
+        params = {}
+        if values is not None:
+            if not param.endswith('.'):
+                param = "%s." % param
+            for num, value in enumerate(values):
+                params['%s%d' % (param, (num + 1))] = value
+        return params
 
-h = hmac.new(
-    six.b(AMAZON_CREDENTIAL['ACCESS_SECRET']),
-    six.b(canonical), hashlib.sha256)
+    def get_competitive_pricing_for_asin(self):
+        self.data['Action'] = 'GetCompetitivePricingForASIN'
+        self.data.update(self.enumerate_param('ASINList.ASIN.', self.asin_list))
+        return self.data
 
-signature = urllib.parse.quote(base64.b64encode(h.digest()), safe='')
+product = Product(ASIN_list)
+print(product.get_competitive_pricing_for_asin())
 
-url = 'https://{}{}?{}&Signature={}'.format(
-    DOMAIN, ENDPOINT, query_string, signature)
+"""
+product = Product(ASIN_list)
+result = [] || {}
+product.get_matching_product_for_id()
+product.get_lowest_offer_listings_for_asin()
+product.get_competitive_pricing_for_asin()
+"""
 
-response = requests.post(url).content.decode()
 
-root = ET.fromstring(response)
 
-ns = {
-        'xmlns':'http://mws.amazonservices.com/schema/Products/2011-10-01',
-        'ns2'  :'http://mws.amazonservices.com/schema/Products/2011-10-01/default.xsd'
-}
 
-for product in root.findall('.//xmlns:Product', ns):
 
-    count = product.find('.//xmlns:OfferListingCount', ns)
-    print('' if count is None else count.text)
+#
+# for num in range(len(ASIN_list)):
+#     index = str(num + 1)
+#     data['ASINList.ASIN.' + index] = ASIN_list[num]
+#
+# query_string = '&'.join('{}={}'.format(
+#     n, urllib.parse.quote(v, safe='')) for n, v in sorted(data.items()))
+#
+# canonical = "{}\n{}\n{}\n{}".format(
+#     'POST', DOMAIN, ENDPOINT, query_string
+# )
+#
+# h = hmac.new(
+#     six.b(AMAZON_CREDENTIAL['ACCESS_SECRET']),
+#     six.b(canonical), hashlib.sha256)
+#
+# signature = urllib.parse.quote(base64.b64encode(h.digest()), safe='')
+#
+# url = 'https://{}{}?{}&Signature={}'.format(
+#     DOMAIN, ENDPOINT, query_string, signature)
+#
+# response = requests.post(url).content.decode()
+#
+# root = ET.fromstring(response)
+#
+# ns = {
+#         'xmlns':'http://mws.amazonservices.com/schema/Products/2011-10-01',
+#         'ns2'  :'http://mws.amazonservices.com/schema/Products/2011-10-01/default.xsd'
+# }
+#
+# for product in root.findall('.//xmlns:Product', ns):
+#
+#     count = product.find('.//xmlns:OfferListingCount', ns)
+#     print('' if count is None else count.text)
+#
+#
+# def get_matching_product_for_id(asin_list:)
