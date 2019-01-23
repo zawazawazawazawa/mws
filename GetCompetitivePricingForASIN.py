dba02904f4ffcba0389f08d65363dceb69a9ea77
zawazawazawazawa <bosse.de.poulet@gmail.com>
2019-01-24 08:13:08 +0900

    Modularized GetMatchingProductForId

--- GetCompetitivePricingForASIN.py
+++ GetCompetitivePricingForASIN.py
@@ -10,6 +10,7 @@ import requests
 import six
 import xml.etree.ElementTree as ET
 import os
+import copy
 
 AMAZON_CREDENTIAL = {
     'SELLER_ID': os.environ['SELLER_ID'],
@@ -17,12 +18,19 @@ AMAZON_CREDENTIAL = {
     'ACCESS_SECRET': os.environ['ACCESS_SECRET']
 }
 
+# max 10
 ASIN_list = ['B07MTZHCKF', 'B07H4KJTC9', 'B01KZB4FF4','B07F85WTS7']
+print(ASIN_list)
 
 class Product:
     # 全APIで使う変数
     DOMAIN   = 'mws.amazonservices.jp'
     ENDPOINT = '/Products/2011-10-01'
+
+    ns = {
+        'xmlns':'http://mws.amazonservices.com/schema/Products/2011-10-01',
+        'ns2'  :'http://mws.amazonservices.com/schema/Products/2011-10-01/default.xsd'
+    }
 
     def datetime_encode(dt):
         return dt.strftime('%Y-%m-%dT%H:%M:%SZ')
@@ -31,7 +39,6 @@ class Product:
 
     data = {
         'AWSAccessKeyId'  : AMAZON_CREDENTIAL['ACCESS_KEY_ID'],
-        # 'Action'          : 'GetCompetitivePricingForASIN',
         'MarketplaceId'   : 'A1VC38T7YXB528',
         'SellerId'        : AMAZON_CREDENTIAL['SELLER_ID'],
         'SignatureMethod' : 'HmacSHA256',
@@ -64,13 +71,123 @@ class Product:
                 params['%s%d' % (param, (num + 1))] = value
         return params
 
-    def get_competitive_pricing_for_asin(self):
-        self.data['Action'] = 'GetCompetitivePricingForASIN'
-        self.data.update(self.enumerate_param('ASINList.ASIN.', self.asin_list))
-        return self.data
+    def get_matching_product_for_id(self):
+        data1 = copy.deepcopy(self.data)
+        data1['Action'] = 'GetMatchingProductForId'
+        data1['IdType'] = 'ASIN'
+        data1.update(self.enumerate_param('IdList.Id.', self.asin_list))
+        print(data1)
+        return self.make_url_and_get_response(data1)
+    #
+    # def get_lowest_offer_listings_for_asin(self):
 
-product = Product(ASIN_list)
-print(product.get_competitive_pricing_for_asin())
+    def get_competitive_pricing_for_asin(self):
+        data3 = copy.deepcopy(self.data)
+        data3['Action']        = 'GetCompetitivePricingForASIN'
+        data3.update(self.enumerate_param('ASINList.ASIN.', self.asin_list))
+        return self.make_url_and_get_response(data3)
+
+    def make_url_and_get_response(self, data):
+        query_string = '&'.join('{}={}'.format(
+            n, urllib.parse.quote(v, safe='')) for n, v in sorted(data.items()))
+
+        canonical = "{}\n{}\n{}\n{}".format(
+            'POST', self.DOMAIN, self.ENDPOINT, query_string
+        )
+
+        h = hmac.new(
+            six.b(AMAZON_CREDENTIAL['ACCESS_SECRET']),
+            six.b(canonical), hashlib.sha256)
+
+        signature = urllib.parse.quote(base64.b64encode(h.digest()), safe='')
+
+        url = 'https://{}{}?{}&Signature={}'.format(
+            self.DOMAIN, self.ENDPOINT, query_string, signature)
+
+        response = requests.post(url).content.decode()
+
+        root = ET.fromstring(response)
+
+        return self.get_list(root)
+
+    def get_list(self, root):
+        count_list = []
+        for product in root.findall('.//xmlns:Product', self.ns):
+            if 'GetCompetitivePricingForASIN' in root.tag:
+                # condition=Any
+                count_node = product.find('.//xmlns:OfferListingCount[@condition="Any"]', self.ns)
+                count = 0 if count_node is None else int(count_node.text)
+                count_list.append(count)
+                return count_list
+
+            elif 'GetMatchingProductForId' in root.tag:
+                result = {}
+                # 商品名
+                title = product.find('.//ns2:Title', self.ns).text
+                result['Title'] = title
+
+                # メーカ名
+                manufacturer = product.find('.//ns2:Manufacturer', self.ns).text
+                result['Manufacturer'] = manufacturer
+
+                # メーカ型番
+                model = product.find('.//ns2:Model', self.ns)
+                result['Model'] = '' if model is None else model.text
+
+                # ブランド名
+                brand = product.find('.//ns2:Brand', self.ns).text
+                result['Brand'] = brand
+
+                # 画像 (サムネ)
+                image_url = product.find('.//ns2:URL', self.ns).text
+                result['Image_URL'] = image_url
+
+                # 画像 (メイン)
+                if '_SL75_.' in image_url:
+                    main_url = image_url.replace('_SL75_.', '')
+                else:
+                    main_url = ''
+                result['Main_image_url'] = main_url
+
+
+                # 商品グループ
+                product_group = product.find('.//ns2:ProductGroup', self.ns).text
+                result['Product Group'] = product_group
+
+                # 高さ (cm)
+                height = product.find('.//ns2:Height', self.ns).text
+                result['Height (inched)'] = height
+
+                # 長さ (cm)
+                length = product.find('.//ns2:Length', self.ns)
+                result['Length (inched)'] = '' if length is None else length.text
+
+                # 幅 (cm)
+                width = product.find('.//ns2:Width', self.ns)
+                result['Width (inched)'] = '' if width is None else width.text
+
+                # 重量 (kg)
+                weight = product.find('.//ns2:Weight', self.ns)
+                result['Weight (pound)'] = '' if weight is None else weight.text
+
+                # 発売日
+                release_date = product.find('.//ns2:ReleaseDate', self.ns)
+                result['Release Date'] = '' if release_date is None else release_date.text
+
+                # 最下位セールスランク
+                rank_dict = {}
+                for category, rank in zip(product.findall('.//xmlns:ProductCategoryId', self.ns), product.findall('.//xmlns:Rank', self.ns)):
+                    rank_dict[category.text] = rank.text
+
+                result.update(rank_dict)
+
+                for header, element in result.items():
+                    print(header, ' : ', element)
+                print('\n')
+
+get_product = Product(ASIN_list)
+# print(product.get_competitive_pricing_for_asin())
+get_product.get_matching_product_for_id()
 
 """
 product = Product(ASIN_list)
@@ -79,45 +196,3 @@ product.get_matching_product_for_id()
 product.get_lowest_offer_listings_for_asin()
 product.get_competitive_pricing_for_asin()
 """
-
-
-
-
-
-#
-# for num in range(len(ASIN_list)):
-#     index = str(num + 1)
-#     data['ASINList.ASIN.' + index] = ASIN_list[num]
-#
-# query_string = '&'.join('{}={}'.format(
-#     n, urllib.parse.quote(v, safe='')) for n, v in sorted(data.items()))
-#
-# canonical = "{}\n{}\n{}\n{}".format(
-#     'POST', DOMAIN, ENDPOINT, query_string
-# )
-#
-# h = hmac.new(
-#     six.b(AMAZON_CREDENTIAL['ACCESS_SECRET']),
-#     six.b(canonical), hashlib.sha256)
-#
-# signature = urllib.parse.quote(base64.b64encode(h.digest()), safe='')
-#
-# url = 'https://{}{}?{}&Signature={}'.format(
-#     DOMAIN, ENDPOINT, query_string, signature)
-#
-# response = requests.post(url).content.decode()
-#
-# root = ET.fromstring(response)
-#
-# ns = {
-#         'xmlns':'http://mws.amazonservices.com/schema/Products/2011-10-01',
-#         'ns2'  :'http://mws.amazonservices.com/schema/Products/2011-10-01/default.xsd'
-# }
-#
-# for product in root.findall('.//xmlns:Product', ns):
-#
-#     count = product.find('.//xmlns:OfferListingCount', ns)
-#     print('' if count is None else count.text)
-#
-#
-# def get_matching_product_for_id(asin_list:)
